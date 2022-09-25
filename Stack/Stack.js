d700581e91951c555bffd4b55ac5e368f6213e5d
Abdsmadi4 <abd.smadi4@gmail.com>
2022-09-25 14:10:38 +0000

    Stack

---
+++ Stack/Stack.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const Node = require("./Node");
+
+class StackUsingLinkedList {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+  }
+
+  push(item) {
+    const newNode =new Node(item);
+
+    newNode.next = this.top;
+
+    this.top = newNode;
+
+    this.length++;
+  }
+
+  pop() {
+    if (!this.isEmpty()) {
+      const poppedItem = this.top.item;
+      this.top =this.top.next;
+      this.length--;
+      return poppedItem;
+    } else {
+      return null;
+    }
+  }
+
+  peek() {
+    if(!this.isEmpty()){
+      return this.top.item;
+    }
+    return null;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  isEmpty() {
+    return this.length === 0 ? true : false;
+  }
+
+  clear() {
+    this.top = null;
+    this.length =0;
+  }
+}
+
+module.exports = StackUsingLinkedList;
